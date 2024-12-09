998b1361be8325cb9aa6655d73ad00abd4311718
EasyPy-dot <visual.starman@gmail.com>
2024-12-09 14:40:15 +0800

    update

---
+++ ProjectInstaller.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Configuration.Install;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace SetupProject
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.AfterInstall += ProjectInstaller_AfterInstall;
+        }
+
+        private void ProjectInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+            //設定etc/hosts
+            if (!IsCheckedMQL5())
+            {
+                using (StreamWriter w = File.AppendText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts")))
+                {
+                    w.WriteLine("#  127.0.0.1  localtest");
+                }
+            }
+        }
+
+        private bool IsCheckedMQL5()
+        {
+            using (StreamReader r = new StreamReader(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts")))
+            {
+                while (r.Peek() >= 0)
+                {
+                    if (r.ReadLine().Contains("localtest"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            var exeConfigurationFileMap = new ExeConfigurationFileMap();
+            exeConfigurationFileMap.ExeConfigFilename = Context.Parameters["assemblypath"] + ".config";
+            var config = ConfigurationManager.OpenMappedExeConfiguration(exeConfigurationFileMap, ConfigurationUserLevel.None);
+            config.AppSettings.Settings["Author"].Value = "QT";
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
